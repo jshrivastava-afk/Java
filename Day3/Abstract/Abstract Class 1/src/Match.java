42b7dd3087a31b4ef7a33e350c9c7a19dd49714e
jshrivastava-afk <78849262+jshrivastava-afk@users.noreply.github.com>
2021-02-26 23:36:42 +0530

    java

---
+++ Day3/Abstract/Abstract Class 1/src/Match.java
@@ -0,0 +1,30 @@
+
+public abstract class Match {
+
+	private int currentscore;
+	private int curentover;
+	private int target;
+	public int getCurrentscore() {
+		return currentscore;
+	}
+	public void setCurrentscore(int currentscore) {
+		this.currentscore = currentscore;
+	}
+	public int getCurentover() {
+		return curentover;
+	}
+	public void setCurentover(int curentover) {
+		this.curentover = curentover;
+	}
+	public int getTarget() {
+		return target;
+	}
+	public void setTarget(int target) {
+		this.target = target;
+	}
+	
+	abstract float calculateRunrate();
+	abstract int calculateBalls();
+	abstract void display(float reqRunrate, int balls);
+	
+}
